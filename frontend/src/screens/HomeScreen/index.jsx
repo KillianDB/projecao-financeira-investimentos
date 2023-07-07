2e0d662ac0f283cad7bf007c7ef64b4c58bb7b3d
KillianDB <killiandomingues57@gmail.com>
2023-07-07 17:33:52 -0300

    melhoras de design home

--- frontend/src/screens/HomeScreen/index.jsx
+++ frontend/src/screens/HomeScreen/index.jsx
@@ -9,15 +9,15 @@ function HomeScreen() {
     return (
         <Fragment>
             <Header title='Profix' />
-            <div class='conteudoHome'>
+            <div className='conteudoHome'>
                 <main>
                 <h1>Planeje seu futuro, projete seus ganhos em renda fixa!</h1>
                 <p>O Profix faz uso da API da Anbima, utilizando da projeção do IPCA da mesma para calcular seus possíveis ganhos no investimento escolhido de renda fixa.</p>
-                <div class='containerButton'>
+                <div className='containerButton'>
                     <button href="/projecao"> Calcule seus ganhos agora</button>
                 </div>
             </main>
-            <div class='animeContainer'>
+            <div className='animeContainer'>
                 <Lottie animationData={EvoluationCoinsAnimation}/>
             </div>
             </div>
